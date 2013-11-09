8eda76b8186bb391efd00f56ac616ddfc87dc189
Therin Irwin <therin.irwin@gmail.com>
2013-11-08 18:26:20 -0800

    Use username instead of full branch name for tabs

--- src/main/groovy/com/entagen/jenkins/BranchView.groovy
+++ src/main/groovy/com/entagen/jenkins/BranchView.groovy
@@ -5,13 +5,23 @@ class BranchView {
     String branchName
 
     public String getViewName() {
-        return "$templateJobPrefix-$safeBranchName"
+        return "$templateJobPrefix-${getUserName()}"
     }
 
     public String getSafeBranchName() {
         return branchName.replaceAll('/', '_')
     }
 
+    public String getUserName() {
+        String[] parts = getSafeBranchName().split('_');
+
+        if (parts.length > 1) {
+            return parts[1];
+        }
+        else {
+            return "no-owner";
+        }
+    }
 
     public String toString() {
         return this.viewName
